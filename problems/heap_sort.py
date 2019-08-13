e48b74df1c8419606115a5475c208ab1df2a9d09
Paco <francisco.merlos.fernandez@gmail.com>
2019-08-13 23:05:52 +0200

    heap sort

---
+++ problems/heap_sort.py
@@ -0,0 +1,8 @@
+"""
+Write a function that takes in an array of integers and returns a sorted
+version of that array. Use the Heap Sort algorithm to sort the array.
+
+"""
+
+def heap_sort(array):
+    pass
