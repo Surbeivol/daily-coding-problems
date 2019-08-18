144859330551e24c57011249a79070e43f1d0c78
Francisco Merlos Fernandez <fmerlos@BME.com>
2019-08-18 18:28:32 +0200

    number of possible binary tree topologies

---
+++ problems/number_possible_binary_topologies.py
@@ -0,0 +1,10 @@
+"""
+Write a function that takes in a non-negative integer n and that returns the number of possible Binary Tree configuration, irrespective of node values. For instance, there exist only two Binary Tree topologies when n is equal to 2: a root node with a left node, and a root node with a right node. Note than wen n is equal to 0, there is one topology that can be created: the None node.
+"""
+
+def num_binary_tree_topologies(numb):
+    pass
+
+# test
+assert num_binary_tree_topologies(3) == 5
+print('OK')
